6b4662a3604187e27b4d4be99113e1d20edb4feb
brunagmoura <64228492+brunagmoura@users.noreply.github.com>
2024-11-13 21:17:21 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.write("Sou servidor público")
 
-numero = st.slider('Selecionar um número', min_value=0, max_value=100, label_visibility="collapsed")
+numero = st.slider('Selecionar um número', min_value=0, max_value=100, label_visibility="hidden")
 st.text(f'Seu número é {numero}')

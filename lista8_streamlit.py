3c5c3bd24551ec0c194fa80e2cd077957093f35b
brunagmoura <64228492+brunagmoura@users.noreply.github.com>
2024-11-13 22:34:49 -0300

    Update lista8_streamlit.py

--- lista8_streamlit.py
+++ lista8_streamlit.py
@@ -1,11 +1,12 @@
 import streamlit as st
 
 #Questão 1:
-st.write("Questão 1")
+st.title("Questão 1")
 st.write("Sou servidor público")
 
+st.divider()
 #Questão 2:
-st.write("Questão 2")
+st.title("Questão 1")
 
 #Aqui vamos usar as formatações de texto padrões do streamlit
 #Eu amo a documentação da página oficial do streamlit, eles trazem vários exemplos e aplicações com imagens:
@@ -32,11 +33,17 @@ st.code("x=2021")
 #E ainda podemos usar latex!
 st.latex(r''' a+a r^1+a r^2+a r^3 ''')
 
+st.divider()
 #Questão 3:
+st.title("Questão 3")
 
 st.write("Slider: ")
 #Usamos min_value como o valor mínimo do nosso seletor e max_value como valor máximo do seletor
+
 numero = st.slider('Selecione um número', min_value = 0, max_value = 100) 
 st.text("Seu número é " + str(numero))
+
+#Outra opção:
+#st.text(f'Seu número é {numero}')
 
 #Questão 4:

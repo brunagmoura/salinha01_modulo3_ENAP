6640ca0787165b1ef07190f27a3baeade3aa89ec
brunagmoura <64228492+brunagmoura@users.noreply.github.com>
2024-11-14 09:29:05 -0300

    Update lista9_aula.py

--- lista9_aula.py
+++ lista9_aula.py
@@ -1,17 +1,28 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.DataFrame({
-    'nomeServidor': ['Adriana', 'Thais', 'Samara'],
-    'salario': [10000, 25000, 20000]
-})
+st.title('Localização das comunidades quilombolas (2022)')
+df = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv')
+df.drop(columns=['Unnamed: 0'], inplace=True)
+list = ['Lat_d', 'Long_d']
+# convertendo para numeros
+df[list] = df[list].apply(pd.to_numeric, errors='coerce')
+estados = df['NM_UF'].unique()
+estadoFiltro = st.selectbox(
+    'Qual estado selecionar?',
+     estados)
+dadosFiltrados = df[df['NM_UF'] == estadoFiltro]
+if st.checkbox('Mostrar tabela'):
+  st.write(dadosFiltrados)
+st.map(dadosFiltrados, latitude="Lat_d", longitude="Long_d")
+qtdeMunicipios = len(df['NM_MUNIC'].unique())
+st.write("A quantidade de municípios com localização quilombola é " + str(qtdeMunicipios))
 
-st.write("Criando uma tabela")
-st.write(df)
-
-opcao = st.selectbox(
-    'Qual servidor você gostaria de selecionar?',
-    options=["Selecione..."] + df['nomeServidor'].tolist()
-)
-
-st.write(opcao)
+qtdeComunidades = len(df['NM_AGLOM'].unique())
+st.write("A quantidade de comunidades quilombolas é " + str(qtdeComunidades))
+st.header('Número de comunidades por UF')
+st.bar_chart(df['NM_UF'].value_counts())
+st.header('Os dez municípios com mais comunidades quilombolas')
+st.bar_chart(df['NM_MUNIC'].value_counts()[:10])
+numero = st.slider('Selecione um número de linhas a serem exibidas', min_value = 0, max_value = 100)
+st.write(df.head(numero))

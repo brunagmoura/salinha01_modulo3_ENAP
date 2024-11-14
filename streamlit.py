4a42734f2de5a1fbe012655e82800e3bed883f49
brunagmoura <64228492+brunagmoura@users.noreply.github.com>
2024-11-13 21:20:15 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -4,3 +4,7 @@ st.write("Sou servidor público")
 
 numero = st.slider('Selecionar um número', min_value=0, max_value=100, label_visibility="hidden")
 st.text(f'Seu número é {numero}')
+
+
+
+st.color_picker('Escolha sua cor favorita')

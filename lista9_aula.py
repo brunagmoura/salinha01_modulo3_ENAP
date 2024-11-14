711326632d0e07e7afd89478f17c21fdbb536345
brunagmoura <64228492+brunagmoura@users.noreply.github.com>
2024-11-14 09:20:23 -0300

    Update lista9_aula.py

--- lista9_aula.py
+++ lista9_aula.py
@@ -2,17 +2,12 @@ import streamlit as st
 import pandas as pd
 
 df = pd.DataFrame({
-    'nomeServidor': ['Adriana', 'Monica', 'Samara'],
-    'salario': [1200,300,5000]
+    'nomeServidor': ['Adriana', 'Thais', 'Samara'],
+    'salario': [10000, 25000, 20000]
 })
 
-st.write("Criando uma tabela!")
+st.write("Criando uma tabela")
 st.write(df)
-opcao = st.selectbox(
-    'Qual servidor você gostaria de selecionar?',
-     df['nomeServidor'])
 
-st.write('Você selecionou: ', opcao)
-
-dadosFiltrados = df[df['nomeServidor'] == opcao]
-dadosFiltrados
+#O selectbox vai selecionar, mas selecionar com base em que?
+st.selectbox('Qual servidor você gostaria de selecionar?', df['nomeServidor'])

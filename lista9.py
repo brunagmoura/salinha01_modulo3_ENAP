9a39cecf0f6d9ae5de16910670cf4cbc1c1987c0
brunagmoura <64228492+brunagmoura@users.noreply.github.com>
2024-11-18 16:34:36 -0300

    Update lista9.py

--- lista9.py
+++ lista9.py
@@ -9,6 +9,8 @@ st.divider()
 
 st.title("Questão 2")
 
+#Confira a explicação detalhada no caderno "Salinha_ProfaBruna_14_11_2024"
+
 df = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv')
 df['Lat_d'] = pd.to_numeric(df['Lat_d'], errors='coerce')
 df['Long_d'] = pd.to_numeric(df['Long_d'], errors='coerce')
@@ -19,15 +21,50 @@ estadoFiltro = st.selectbox(
 dadosFiltrados = df[df['NM_UF'] == estadoFiltro]
 st.map(dadosFiltrados, latitude="Lat_d", longitude="Long_d")
 
+#Podemos exibir a quantidade de municípios no Estado selecionado usando a função len(), que mede a quantidade de linhas
+#de um dataframe
+#Somado a ele, vamos usar o unique, que informa quantas ocorrências únicas existe em uma coluna
 
 qtdeMunicipios = len(df['NM_MUNIC'].unique())
 st.write("A quantidade de municípios com localização quilombola é " + str(qtdeMunicipios))
 
 qtdeComunidades = len(df['NM_AGLOM'].unique())
 st.write("A quantidade de comunidades quilombolas é " + str(qtdeComunidades))
-st.header('Número de comunidades por UF')
+
+st.header('Número de comunidades por UF - usando a função nativa do streamlit')
 st.bar_chart(df['NM_UF'].value_counts())
-st.header('Os dez municípios com mais comunidades quilombolas')
+
+st.header('Número de comunidades por UF - usando o plotly')
+df_contagem = df['NM_UF'].value_counts().reset_index()
+import plotly.express as px
+fig = px.bar(
+    df_contagem,
+    x='NM_UF',
+    y='count',
+    title='Distribuição por Unidade Federativa',
+    labels={'NM_UF': 'Unidade Federativa', 'count': 'Número de Registros'},
+    text='count'  # Exibir os valores em cada barra
+)
+st.plotly_chart(fig)
+
+#Podemos usar a função nativa do streamlit para fazer bar_chart
+st.header('Os dez municípios com mais comunidades quilombolas - usando a função nativa do streamlit')
 st.bar_chart(df['NM_MUNIC'].value_counts()[:10])
+
+
+st.header('Os dez municípios com mais comunidades quilombolas - usando o plotly')
+df_contagem_municipios = df['NM_MUNIC'].value_counts()[:10].reset_index()
+fig_municipios = px.bar(
+    df_contagem_municipios,
+    x='NM_MUNIC',
+    y='count',
+    title='Distribuição por Unidade Federativa',
+    labels={'NM_MUNIC': 'Unidade Federativa', 'count': 'Número de Registros'},
+    text='count'  # Exibir os valores em cada barra
+)
+st.plotly_chart(fig_municipios)
+
+
+
 numero = st.slider('Selecione um número de linhas a serem exibidas', min_value = 0, max_value = 100)
 st.write(df.head(numero))

bffd846e5a2c408d6e0600a3b4145a6367379363
brunagmoura <64228492+brunagmoura@users.noreply.github.com>
2024-11-18 11:51:33 -0300

    Update stackedbar.py

--- stackedbar.py
+++ stackedbar.py
@@ -12,20 +12,27 @@ response_homens = requests.get(url_homens)
 data_mulheres = response_mulheres.json()
 data_homens = response_homens.json()
 
-df_mulheres = pd.DataFrame(data_mulheres['dados'])
-df_homens = pd.DataFrame(data_homens['dados'])
+df_mulheres = df_mulheres['siglaUf'].value_counts().reset_index()
+#Temos que contar a quantidade de mulheres por UF
+df_mulheres.columns = ['UF', 'Quantidade']
+df_mulheres['Sexo'] = 'Feminino'
+
+df_homens = df_homens['siglaUf'].value_counts().reset_index()
+#Temos que contar a quantidade de homens por UF
+df_homens.columns = ['UF', 'Quantidade']
+df_homens['Sexo'] = 'Masculino'
 
 df = pd.concat([df_mulheres, df_homens])
 
 st.write(df)
 
-#fig = px.bar(
-#    df,
-#    x='UF',
-#    y='Quantidade',
-#    color='Sexo',
-#    labels={'Quantidade': 'Quantidade', 'UF': 'Unidade Federativa'},
-#    barmode='stack'  # Define o gráfico como empilhado
-#)
+fig = px.bar(
+    df,
+    x='UF',
+    y='Quantidade',
+    color='Sexo',
+    labels={'Quantidade': 'Quantidade', 'UF': 'Unidade Federativa'},
+    barmode='stack'  # Define o gráfico como empilhado
+)
 
-#st.plotly_chart(fig)
+st.plotly_chart(fig)
